a5bd826e08b645119a1800044d9df7288f28383a
Dani Renner <danijrenner@gmail.com>
2021-04-23 13:02:41 -0700

    handle adding new keg to list

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import KegDetail from './KegDetail';
 import KegList from './KegList';
+import NewKegForm from './NewKegForm';
 
 class KegControl extends React.Component {
 
@@ -13,9 +14,28 @@ class KegControl extends React.Component {
     };
   }
 
+  handleClick = () => {
+    if (this.state.selectedKeg != null){
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
   handleChangingSelectedKeg = (id) => {
     const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
     this.setState({selectedKeg: selectedKeg});
+  }
+
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({masterKegList: newMasterKegList,
+    formVisibleOnPage: false });
   }
   
   render(){
@@ -24,7 +44,10 @@ class KegControl extends React.Component {
     if (this.state.selectedKeg != null){
     currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg}  />
     buttonText = "Return to Keg List";
-    }else {
+    }else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
+      buttonText = "Return to Keg List";
+    } else {
       currentlyVisibleState = <KegList kegList={this.state.masterKegList} onKegSelection={this.handleChangingSelectedKeg} />
       buttonText = "Add Keg";
     }
---
+++ src/components/NewKegForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+
+function NewKegForm(props){
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({name: event.target.name.value, brewer: event.target.brewer.value, price: event.target.price.value, id: v4()});
+  }
+
+  return (
+    <React.Fragment>
+        <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add to Tap Room" />
+    </React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
