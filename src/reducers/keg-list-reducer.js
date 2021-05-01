466bc0c599003343f9da75b116cc18cff929f819
Dani Renner <danijrenner@gmail.com>
2021-05-01 10:02:01 -0700

    add 'ADD_KEG' case to list reducer, test pass

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brewer, price, abv, pints, id } = action;
+  switch (action.type) {
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brewer: brewer,
+        price: price,
+        abv: abv,
+        pints: pints,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
