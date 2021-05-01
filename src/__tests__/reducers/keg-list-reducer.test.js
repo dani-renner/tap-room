efc6ab4110be50f7bfac1df98ce285c8d5ff3485
Dani Renner <danijrenner@gmail.com>
2021-05-01 09:48:59 -0700

    add example keg data for testing

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,15 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    name: 'Crikey IPA',
+    brewer: "Reuben's",
+    price: 'Redux action is not working correctly.',
+    abv: '6.8%',
+    pints: '140'
+    id: 1
+  };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
