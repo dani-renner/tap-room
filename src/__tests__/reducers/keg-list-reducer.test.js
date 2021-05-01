e4ca2ed31c1229f1129713942d949116b1391f56
Dani Renner <danijrenner@gmail.com>
2021-05-01 09:58:46 -0700

    destructure data for 'add' test, good fail

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -5,12 +5,36 @@ describe('kegListReducer', () => {
   const kegData = {
     name: 'Crikey IPA',
     brewer: "Reuben's",
-    price: 'Redux action is not working correctly.',
+    price: '$165',
     abv: '6.8%',
-    pints: '140'
+    pints: '140',
     id: 1
   };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brewer, price, abv, pints, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brewer: brewer,
+      price: price,
+      abv: abv,
+      pints: pints,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brewer: brewer,
+        price: price,
+        abv: abv,
+        pints: pints,
+        id: id
+      }
+    });
   });
 });
